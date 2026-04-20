721395704fe06a3235842dcc66496a2308f4b185
Dilip <dilipkumar91381@gmail.com>
2026-04-20 13:12:37 +0530

    first commit

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,25 @@
+const Task = require("../models/Task");
+
+// GET
+exports.getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+};
+
+// POST
+exports.createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.json(task);
+};
+
+// PUT
+exports.updateTask = async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+};
+
+// DELETE
+exports.deleteTask = async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+};
